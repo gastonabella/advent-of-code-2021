b00a365ae3e39022dfc9694fbb92c601f10da957
Gastón Abellá <gaston@wyeworks.com>
2021-12-20 16:05:46 -0300

    Solution 2 - Day 2

--- day_2/day_2.exs
+++ day_2/day_2.exs
@@ -10,6 +10,15 @@ defmodule Day2 do
     |> (fn {x, y} -> x * y end).()
   end
 
+  def solution2 do
+    start_stream()
+    |> Stream.scan({0, 0, 0}, fn command, pos ->
+      execute_with_aim(command, pos)
+    end)
+    |> last_from_stream()
+    |> (fn {_acc, x, y} -> x * y end).()
+  end
+
   defp cast(n) do
     n |> String.trim() |> String.to_integer()
   end
@@ -24,6 +33,15 @@ defmodule Day2 do
   defp execute("down" <> steps, {x, y}), do: {x, y + cast(steps)}
   defp execute("up" <> steps, {x, y}), do: {x, y - cast(steps)}
 
+  defp execute_with_aim("forward" <> steps, {aim, x, y}) do
+    steps = cast(steps)
+
+    {aim, x + steps, y + aim * steps}
+  end
+
+  defp execute_with_aim("down" <> steps, {aim, x, y}), do: {aim + cast(steps), x, y}
+  defp execute_with_aim("up" <> steps, {aim, x, y}), do: {aim - cast(steps), x, y}
+
   defp last_from_stream(stream) do
     stream
     |> Stream.take(-1)
@@ -33,3 +51,4 @@ defmodule Day2 do
 end
 
 Day2.solution1() |> IO.puts()
+Day2.solution2() |> IO.puts()
